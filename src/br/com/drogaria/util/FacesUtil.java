1192e5923dc9383c3fda1d226a0f0ec92419bdc4
Andre Marinho <andre.bmarinho@hotmail.com>
2017-12-08 05:50:32 -0300

    Flash Scope - Fazendo que as mensagens via growl  fiquem vivas durante a mais de uma requisição.

--- src/br/com/drogaria/util/FacesUtil.java
+++ src/br/com/drogaria/util/FacesUtil.java
@@ -5,31 +5,42 @@ import java.util.Map;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.faces.context.Flash;
 
 public class FacesUtil {
-	
-	public static void adicionarMsgInfo(String mensagem){
+
+	public static void adicionarMsgInfo(String mensagem) {
 		FacesMessage facesMessage = new FacesMessage(FacesMessage.SEVERITY_INFO, mensagem, mensagem);
 		FacesContext facesContext = FacesContext.getCurrentInstance();
+
+		ExternalContext externalContext = facesContext.getExternalContext();
+
+		Flash flash = externalContext.getFlash();
+		flash.setKeepMessages(true);
+
 		facesContext.addMessage(null, facesMessage);
 	}
-	
-	public static void adicionarMsgError(String mensagem){
+
+	public static void adicionarMsgError(String mensagem) {
 		FacesMessage facesMessage = new FacesMessage(FacesMessage.SEVERITY_ERROR, mensagem, mensagem);
 		FacesContext facesContext = FacesContext.getCurrentInstance();
+
+		ExternalContext externalContext = facesContext.getExternalContext();
+
+		Flash flash = externalContext.getFlash();
+		flash.setKeepMessages(true);
+
+		facesContext.addMessage(null, facesMessage);
+
 		facesContext.addMessage(null, facesMessage);
 	}
-	
-	
-	public static String getParam(String nome){
+
+	public static String getParam(String nome) {
 		FacesContext facesContext = FacesContext.getCurrentInstance();
 		ExternalContext externalContext = facesContext.getExternalContext();
 		Map<String, String> parametro = externalContext.getRequestParameterMap();
 		String valor = parametro.get(nome);
 		return valor;
 	}
-	
-	
-	
 
 }
