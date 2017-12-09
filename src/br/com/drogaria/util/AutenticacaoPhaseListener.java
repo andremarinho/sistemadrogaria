26ccdfe8a17b6a4dac50f6c025d4e1de40655faa
Andre Marinho <andre.bmarinho@hotmail.com>
2017-12-09 07:33:00 -0300

    Phase Listener - Parte 1

---
+++ src/br/com/drogaria/util/AutenticacaoPhaseListener.java
@@ -0,0 +1,34 @@
+package br.com.drogaria.util;
+
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.FacesContext;
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
+
+@SuppressWarnings("serial")
+public class AutenticacaoPhaseListener implements PhaseListener {
+
+	@Override
+	public void afterPhase(PhaseEvent event) {
+		FacesContext facesContext = event.getFacesContext();
+		UIViewRoot uiViewRoot = facesContext.getViewRoot();
+		String paginaAtual = uiViewRoot.getViewId();
+		boolean ehPaginaAutenticacao = paginaAtual.contains("autenticacao");
+		System.out.println("É pagina de autenticacação "+ehPaginaAutenticacao);
+		
+	}
+
+	@Override
+	public void beforePhase(PhaseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public PhaseId getPhaseId() {
+		
+		return PhaseId.RESTORE_VIEW;
+	}
+
+}
