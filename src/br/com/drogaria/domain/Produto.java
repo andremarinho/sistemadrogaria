d96874c3db0eb58c147299eb020ba3ba82d1d058
Andre Marinho <andre.bmarinho@hotmail.com>
2017-12-10 07:34:02 -0300

    Implementado conversor de numero para resolver o problema de visualização dos preços

--- src/br/com/drogaria/domain/Produto.java
+++ src/br/com/drogaria/domain/Produto.java
@@ -15,6 +15,7 @@ import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.validation.constraints.DecimalMax;
 import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Digits;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
@@ -43,6 +44,7 @@ public class Produto {
 	@NotNull(message="O campo preço é obrigatorio!")
 	@DecimalMin(value="0.00", message="Informe um valor maior ou igual a zero para o compo preço.")
 	@DecimalMax(value="99999.99",message="Informe um valor menor que R$ 99999,99 para o campo preço.")
+	@Digits(integer=5, fraction=2, message="O preço só aceita duas casas após a virgula.")
 	@Column(name = "pro_preco", precision = 7, scale = 2, nullable = false)
 	private BigDecimal preco;
 
